74f612e8d9bcb220c605ab15d5384cc27eded32e
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-19 17:56:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const userName = prompt ('Как Вас зовут?');
+console.log('lower', userName.toLowerCase().trim());
+const userAge = prompt('Сколько Вам лет?');
+console.log('number user age', userAge.trim());
+alert('Вас зовут,'+ userName);
+alert('Ваш возраст,' + userAge );
